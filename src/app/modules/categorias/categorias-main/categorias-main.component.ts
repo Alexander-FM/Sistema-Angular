b49ec6a517b8b8dd57a66ec6640285f43487f816
lfuenmed <luiguialexander.fuentesmedina@everis.nttdata.com>
2023-06-14 23:25:03 -0500

    new changes v11

---
+++ src/app/modules/categorias/categorias-main/categorias-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-main',
+  templateUrl: './categorias-main.component.html',
+  styleUrls: ['./categorias-main.component.css']
+})
+export class CategoriasMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
