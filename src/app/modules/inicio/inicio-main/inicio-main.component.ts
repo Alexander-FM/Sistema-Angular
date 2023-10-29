6460c26da1bac958751de7b719c6471a371f3afb
Wilson Hernande Neira Mija <WILSONHNM@ext.zara.com>
2023-10-29 00:06:11 -0500

    Se implemento web sockets

--- src/app/modules/inicio/inicio-main/inicio-main.component.ts
+++ src/app/modules/inicio/inicio-main/inicio-main.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {WebSocket} from '../../../shared/services/webSocket';
+import {Message, MessageService} from 'primeng-lts/api';
 
 @Component({
   selector: 'app-inicio-main',
   templateUrl: './inicio-main.component.html',
   styleUrls: ['./inicio-main.component.css']
 })
-export class InicioMainComponent implements OnInit {
+export class InicioMainComponent implements OnInit, OnDestroy {
+  mostrar = false;
+  user: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(protected webSocketService: WebSocket, private messageService: MessageService) {
   }
 
+  ngOnInit() {
+    this.listeningSocket();
+  }
+
+  listeningSocket() {
+    this.webSocketService.connect();
+    this.webSocketService.response$.subscribe((data) => {
+      console.log('Datos recibidos', data);
+      const audio = new Audio('assets/audio/calendar.mp3');
+      audio.play().then(r => {
+      });
+      this.mostrar = true;
+      this.user = data.pedido.cliente.nombreCompletoCliente;
+      setTimeout(() => {
+        this.mostrar = false;
+        this.messageService.clear();
+      }, 8000);
+    });
+  }
+
+  showSuccess() {
+    this.messageService.add({severity: 'info', summary: 'Info Message', detail: 'PrimeNG rocks'});
+
+  }
+
+  ngOnDestroy() {
+    this.webSocketService.disconnect();
+  }
 }
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -3,13 +3,16 @@ import { InicioMainComponent } from './inicio-main/inicio-main.component';
 import { InicioRoutingModule } from './inicio-routing.module';
 import {DialogModule} from 'primeng-lts/dialog';
 import {SharedModule} from '../../shared/shared.module';
+import {WebSocket} from '../../shared/services/webSocket';
+import {MessageService} from 'primeng-lts/api';
 
 @NgModule({
   declarations: [
     InicioMainComponent
   ],
   providers: [
-
+    WebSocket,
+    MessageService
   ],
   imports: [
     InicioRoutingModule,
